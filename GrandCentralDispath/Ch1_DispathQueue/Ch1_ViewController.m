67ce49baf5c33963f5e6482cd1e60615f179a1e8
fjq <530743460@qq.com>
2017-12-25 17:49:38 +0800

    Dispatch Queue

--- GrandCentralDispath/Ch1_DispathQueue/Ch1_ViewController.m
+++ GrandCentralDispath/Ch1_DispathQueue/Ch1_ViewController.m
@@ -16,7 +16,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    //开发者要做的只是定义想执行的任务并追加到适当的 Dispatch Queue中；
+    //例1
+    dispatch_queue_t queue = dispatch_get_main_queue();
+    
+    dispatch_async(queue, ^{
+        /*
+         想要执行的任务
+         长时间处理
+         例如AR用画像识别
+         例如数据库访问
+         */
+        NSLog(@"这个是例子1");
+    });
+    
+    // Dispatch Queue的种类
+    // Serial Dispatch Queue。等待现在执行中处理结束（串行）
+    // Concurrent Dispath Queue 不等现在执行中处理结束（并发）
+    
+    //例2 串行队列
+    dispatch_queue_t serialQueue = dispatch_queue_create("serialQueue", DISPATCH_QUEUE_SERIAL);
+    
+    dispatch_async(serialQueue, ^{
+        NSLog(@"serialQueue0");
+    });
+    dispatch_async(serialQueue, ^{
+        NSLog(@"serialQueue1");
+    });
+    dispatch_async(serialQueue, ^{
+        NSLog(@"serialQueue2");
+    });
+    dispatch_async(serialQueue, ^{
+        NSLog(@"serialQueue3");
+    });
+    dispatch_async(serialQueue, ^{
+        NSLog(@"serialQueue4");
+    });
+    
+    //例3 并发队列
+    dispatch_queue_t concurrentQueue = dispatch_queue_create("concurrentQueue", DISPATCH_QUEUE_CONCURRENT);
+    
+    dispatch_async(concurrentQueue, ^{
+        NSLog(@"concurrentQueue0");
+    });
+    dispatch_async(concurrentQueue, ^{
+        NSLog(@"concurrentQueue1");
+    });
+    dispatch_async(concurrentQueue, ^{
+        NSLog(@"concurrentQueue2");
+    });
+    dispatch_async(concurrentQueue, ^{
+        NSLog(@"concurrentQueue3");
+    });
+    dispatch_async(concurrentQueue, ^{
+        NSLog(@"concurrentQueue4");
+    });
+
 }
 
 - (void)didReceiveMemoryWarning {
