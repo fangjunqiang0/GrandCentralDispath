c2fe5ca7e6ff35a552f489f81a0b09241af991ad
fjq <530743460@qq.com>
2017-12-26 20:10:30 +0800

    新增dispatch_queue_create() 相关说明

--- GrandCentralDispath/Ch1_DispathQueue/Ch1_ViewController.m
+++ GrandCentralDispath/Ch1_DispathQueue/Ch1_ViewController.m
@@ -17,10 +17,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //开发者要做的只是定义想执行的任务并追加到适当的 Dispatch Queue中；
-    //例1
-    dispatch_queue_t queue = dispatch_get_main_queue();
+    //苹果官方对GCD的说明：开发者要做的只是定义想执行的任务并追加到适当的 Dispatch Queue中 例子1
+    [self mainQueue];
     
+    // Dispatch Queue的种类
+    // Serial Dispatch Queue。等待现在执行中处理结束（串行）例子2
+    [self serialQueue];
+    
+    
+    // Concurrent Dispath Queue 不等现在执行中处理结束（并发）例子3
+    [self concurrentQueue];
+    
+   /*
+    总结
+    本例子，主要说明三个问题：
+    1、Dispatch Queue 是什么？字面意思是“派发队列”，如字面意思，是执行处理的的等待队列。开发人员通过dispatch_aysnc等函数API，在Block里面记述想执行处理并追加到Dispatch queue中。Dispatch Queue按照追加的顺序（先进先出FIFO，first in first out）执行处理。例子1
+    2、串行队列，就是等前一个队列执行完毕才执行下一个队列。我们可以通过例子2的打印结果，看出serialQueue0-9的打印顺序是正序排列的。
+    3、并发队列，每一个队列执行不用等待其他队列是否执行完毕。我们可以通过例子3的打印结果，看出concurrentQueue0-9的打印顺序是混乱的。
+    注：
+    串行队列使用一个线程
+    并发队列使用多个线程，XNU核心决定应当使用的线程数量，执行顺序回根据执行内容及系统状态发生改变。（多次执行例子3即可看出）
+    dispatch_async()、dispatch_get_main_queue()、dispatch_queue_create()此处不做解释。
+    */
+    
+
+}
+
+/**
+ 例1
+ */
+- (void)mainQueue {
+    
+    dispatch_queue_t queue = dispatch_get_main_queue();
     dispatch_async(queue, ^{
         /*
          想要执行的任务
@@ -30,14 +58,15 @@
          */
         NSLog(@"这个是例子1");
     });
-    
-    // Dispatch Queue的种类
-    // Serial Dispatch Queue。等待现在执行中处理结束（串行）
-    // Concurrent Dispath Queue 不等现在执行中处理结束（并发）
-    
-    //例2 串行队列
-    dispatch_queue_t serialQueue = dispatch_queue_create("serialQueue", DISPATCH_QUEUE_SERIAL);
-    
+}
+
+/**
+ 例2 串行队列
+ */
+- (void)serialQueue {
+    //参数DISPATCH_QUEUE_SERIAL 表示生成的队列为串行队列
+    dispatch_queue_t serialQueue = dispatch_queue_create("com.GrandCentralDispath.serialQueue", DISPATCH_QUEUE_SERIAL);
+
     dispatch_async(serialQueue, ^{
         NSLog(@"serialQueue0");
     });
@@ -53,9 +82,29 @@
     dispatch_async(serialQueue, ^{
         NSLog(@"serialQueue4");
     });
-    
-    //例3 并发队列
-    dispatch_queue_t concurrentQueue = dispatch_queue_create("concurrentQueue", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(serialQueue, ^{
+        NSLog(@"serialQueue5");
+    });
+    dispatch_async(serialQueue, ^{
+        NSLog(@"serialQueue6");
+    });
+    dispatch_async(serialQueue, ^{
+        NSLog(@"serialQueue7");
+    });
+    dispatch_async(serialQueue, ^{
+        NSLog(@"serialQueue8");
+    });
+    dispatch_async(serialQueue, ^{
+        NSLog(@"serialQueue9");
+    });
+}
+
+/**
+ 例3 并发队列
+ */
+- (void)concurrentQueue {
+    //DISPATCH_QUEUE_CONCURRENT 表示生成的队列为并发队列
+    dispatch_queue_t concurrentQueue = dispatch_queue_create("com.GrandCentralDispath.concurrentQueue", DISPATCH_QUEUE_CONCURRENT);
     
     dispatch_async(concurrentQueue, ^{
         NSLog(@"concurrentQueue0");
@@ -72,8 +121,23 @@
     dispatch_async(concurrentQueue, ^{
         NSLog(@"concurrentQueue4");
     });
-
+    dispatch_async(concurrentQueue, ^{
+        NSLog(@"concurrentQueue5");
+    });
+    dispatch_async(concurrentQueue, ^{
+        NSLog(@"concurrentQueue6");
+    });
+    dispatch_async(concurrentQueue, ^{
+        NSLog(@"concurrentQueue7");
+    });
+    dispatch_async(concurrentQueue, ^{
+        NSLog(@"concurrentQueue8");
+    });
+    dispatch_async(concurrentQueue, ^{
+        NSLog(@"concurrentQueue9");
+    });
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
--- GrandCentralDispath/Ch2_dispath_queue_create/Ch2_ViewController.m
+++ GrandCentralDispath/Ch2_dispath_queue_create/Ch2_ViewController.m
@@ -16,9 +16,99 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    //获取Dispatch queue的方法有两种，分别为通过GCD的API和系统提供的。这里讲解通过GCD的API方式，系统提供的方式在“Ch3_MainDispatchQueue”讲解
+    
+    //例子1 通过dispatch_queue_crete()函数生成Dispatch Queue
+    [self serialDispathQueueCreate];
+    /*
+     注：关于Serial Dispatch Queue生成个数的问题
+     通过dispatch_queue_crete()生成多个Serial Dispatch Queue时，各个Serial Dispatch Queue将并行执行（并发队列）,查看例子1的打印日志
+     */
+    
+    //例子2 dispatch_queue_crete()函数 参数说明
+    [self dispatchQueueCreateParameter];
+    /*
+     注：关于线程的内存管理 iOS7以后的系统ARC自动管理
+     释放：dispatch_release()  保留：dispath_retain()
+     */
+    
 }
 
+/**
+ 例子1
+ */
+- (void)serialDispathQueueCreate {
+    //dispatch_queue_create() 函数使用
+    dispatch_queue_t mySerialQueue0 = dispatch_queue_create("com.GrandCentralDispath.mySerialQueue0", DISPATCH_QUEUE_SERIAL);
+    dispatch_queue_t mySerialQueue1 = dispatch_queue_create("com.GrandCentralDispath.mySerialQueue1", DISPATCH_QUEUE_SERIAL);
+    dispatch_queue_t mySerialQueue2 = dispatch_queue_create("com.GrandCentralDispath.mySerialQueue2", DISPATCH_QUEUE_SERIAL);
+    dispatch_queue_t mySerialQueue3 = dispatch_queue_create("com.GrandCentralDispath.mySerialQueue3", DISPATCH_QUEUE_SERIAL);
+    dispatch_queue_t mySerialQueue4 = dispatch_queue_create("com.GrandCentralDispath.mySerialQueue4", DISPATCH_QUEUE_SERIAL);
+    dispatch_queue_t mySerialQueue5 = dispatch_queue_create("com.GrandCentralDispath.mySerialQueue5", DISPATCH_QUEUE_SERIAL);
+    dispatch_queue_t mySerialQueue6 = dispatch_queue_create("com.GrandCentralDispath.mySerialQueue6", DISPATCH_QUEUE_SERIAL);
+    dispatch_queue_t mySerialQueue7 = dispatch_queue_create("com.GrandCentralDispath.mySerialQueue7", DISPATCH_QUEUE_SERIAL);
+    dispatch_queue_t mySerialQueue8 = dispatch_queue_create("com.GrandCentralDispath.mySerialQueue8", DISPATCH_QUEUE_SERIAL);
+    dispatch_queue_t mySerialQueue9 = dispatch_queue_create("com.GrandCentralDispath.mySerialQueue9", DISPATCH_QUEUE_SERIAL);
+    
+    dispatch_async(mySerialQueue0, ^{
+        NSLog(@"mySerialQueue0");
+    });
+    dispatch_async(mySerialQueue1, ^{
+        NSLog(@"mySerialQueue1");
+    });
+    dispatch_async(mySerialQueue2, ^{
+        NSLog(@"mySerialQueue2");
+    });
+    dispatch_async(mySerialQueue3, ^{
+        NSLog(@"mySerialQueue3");
+    });
+    dispatch_async(mySerialQueue4, ^{
+        NSLog(@"mySerialQueue4");
+    });
+    dispatch_async(mySerialQueue5, ^{
+        NSLog(@"mySerialQueue5");
+    });
+    dispatch_async(mySerialQueue6, ^{
+        NSLog(@"mySerialQueue6");
+    });
+    dispatch_async(mySerialQueue7, ^{
+        NSLog(@"mySerialQueue7");
+    });
+    dispatch_async(mySerialQueue8, ^{
+        NSLog(@"mySerialQueue8");
+    });
+    dispatch_async(mySerialQueue9, ^{
+        NSLog(@"mySerialQueue9");
+    });
+}
+
+/**
+ 例子2
+ */
+- (void)dispatchQueueCreateParameter {
+    /*
+     dispatch_queue_create() 函数参数说明
+     第一参数，指定Dispatch queue 的名称，推荐使用应用程序ID这种全程域名,例如："com.GrandCentralDispath.mySerialQueue0"。改名称在Xcode和Instruments的调试器中作为Dispatch Queue名称表示。（可加一个断点查看）。
+     第二个参数，传NUll或DISPATCH_QUEUE_SERIAL 生成 Serial Dispatch Queue（串行队列）
+            传DISPATCH_QUEUE_CONCURRENT 生成 Concurrent Dispatch Queue（并发队列）
+     */
+    
+    dispatch_queue_t mySerialQueue10 = dispatch_queue_create("com.GrandCentralDispath.mySerialQueue10", NULL);//串行队列
+    dispatch_queue_t mySerialQueue11 = dispatch_queue_create("com.GrandCentralDispath.mySerialQueue11", DISPATCH_QUEUE_SERIAL);//串行队列
+    dispatch_queue_t myConcurrentQueue0 = dispatch_queue_create("com.GrandCentralDispath.myConcurrentQueue0", DISPATCH_QUEUE_CONCURRENT);//并发队列
+    
+    dispatch_async(mySerialQueue10, ^{
+        NSLog(@"mySerialQueue10");
+        
+    });
+    dispatch_async(mySerialQueue11, ^{
+        NSLog(@"mySerialQueue11");
+    });
+    dispatch_async(myConcurrentQueue0, ^{
+        NSLog(@"myConcurrentQueue0");
+    });
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
